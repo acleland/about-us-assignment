1c216c6f742acf5c802de268414e21c11b49bdc9
Andrea Cleland <andreaclelan@gmail.com>
2022-01-19 16:33:08 -0800

    added data.js file with our data

---
+++ data.js
@@ -0,0 +1,14 @@
+export const people = [
+    {
+        id: 1,
+        name: 'Andrea',
+        bio: 'Coder, physics fan, dinosaur enthusiast',
+        hobbies: ['video games', 'hiking', 'travel']
+    },
+    {
+        id: 2,
+        name: 'Alex',
+        bio: 'Just here for the ride',
+        hobbies: ['Shredding', 'relaxing', 'video games']
+    }
+];

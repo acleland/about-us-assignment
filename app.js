b9f59b806c1a59117eb5d2d6a693611e6c02e86f
Alex Orlet <AlexOrlet89@gmail.com>
2022-01-20 12:01:59 -0800

    selected elements from DOM

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions
-
+import { people } from './data.js';
+// console.log(people, 'people');
 // grab DOM elements
-
+const selectEl = document.querySelector('.select');
+const nameEl = document.querySelector('.name');
+const ageEl = document.querySelector('.age');
+const bioEl = document.querySelector('.bio');
+const hobbiesEl = document.querySelector('.hobbies');
+const containEl = document.querySelector('.contain');
+console.log(selectEl, nameEl, ageEl, bioEl, hobbiesEl, containEl);
+// const 
 // set event listeners 
+selectEl.addEventListener('change', (e) => {
+    console.log('user click');
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state

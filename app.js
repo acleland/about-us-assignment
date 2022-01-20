ec3a4a8ac91518f3da16fd412aed89dfcc02f90f
Andrea Cleland <andreaclelan@gmail.com>
2022-01-20 12:19:21 -0800

    created renderPerson function

--- app.js
+++ app.js
@@ -13,7 +13,23 @@ console.log(selectEl, nameEl, ageEl, bioEl, hobbiesEl, containEl);
 // set event listeners 
 selectEl.addEventListener('change', (e) => {
     console.log('user click');
+
 });
+
+function renderPerson(index) {
+    // do stuff to dom to render person
+    containEl.classList.add(`${people[index].name}`);
+    nameEl.textContent = 'Name: ' + people[index].name;
+    ageEl.textContent = 'Age: ' + people[index].age;
+    bioEl.textContent = 'Bio: ' + people[index].bio;
+
+    // hobbies
+    for (let hobby of people[index].hobbies) {
+        const li = document.createElement('li');
+        li.textContent = hobby;
+        hobbiesEl.appendChild(li);
+    }
+}
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
